b9cc85f23a9c63322e58899a116025af45a7a8be
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2023-03-08 12:56:30 +0000

    Last commit

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-//create database
-const database = {
-  Monday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump','ride', 'run', 'jump']},
-  Tuesday:  {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump','ride', 'run', 'jump']},
-  Wednesday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
-  Thursday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
-  Friday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
-  Saturday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
-  Sunday: {
-  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
-  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
-  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+const prompt = require('prompt')
 
-}
-console.log(database.Monday.verb[0])
+prompt.start()
+
+//create database
+const database = [
+  {day: 'Monday' ,
+  verb:['ride', 'run', 'jump', 'dance', 'eat'] ,
+  adjective: ['dead', 'glorious', 'beautiful', 'slim', 'destroyed'],
+  noun: ['car', 'dog', 'house', 'tree', 'cat']},
+  {day: 'Tuesday',
+  verb:['play football', 'play poker', 'play video games', 'work', 'build'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing'],
+  noun: ['bicycle', 'school', 'tomato', 'banana', 'president']},
+  {day: 'Wednesday',
+  verb:['study', 'sleep', 'work out', 'zumba'] ,
+  adjective: ['clever', 'histrionic', 'histerical', 'pompous', 'glamorous'],
+  noun: ['laptop', 'loop', 'conditional', 'function', 'array']},
+  {day: 'Thursday',
+  verb:['create', 'code', 'drive', 'devour', 'shag'] ,
+  adjective: ['redish', 'dark', 'bright', 'slimmy', 'sticky'],
+  noun: ['toilet paper', 'poo', 'steak', 'sandwich', 'cementery']},
+  {day: 'Friday',
+  verb:['sneeze', 'squeeze', 'press', 'push', 'pull'] ,
+  adjective: ['stupid', 'pretentious', 'adorable', 'athletic', 'sexy'],
+  noun: ['batman', 'mug', 'flower', 'cake', 'bus']},
+  {day: 'Saturday',
+  verb:['collect', 'disrupt', 'destroy', 'pinch', 'grab'] ,
+  adjective: ['new', 'old', 'ancient', 'stinky', 'retarded'],
+  noun: ['kite', 'waves', 'sharks', 'cartoons', 'tables']},
+  {day: 'Sunday',
+  verb:['slide', 'paraglide', 'stroke', 'kiss', 'film'] ,
+  adjective: ['fit', 'detremined', 'strange', 'spooky', 'scary'],
+  noun: ['plains', 'doritos', 'teeth', 'leegs', 'hands']},
+  ]
+
+rand = Math.floor(Math.random() * 4)
+console.log('When prompted type name and favorite day of the week: ')
+prompt.get(['nam', 'day'], function (err, result){
+  dia = result.day
+  nam = result.nam
+console.log(rand)
+let verb = 0
+let adjective = 0
+let noun = 0
+for (let i = 0; i < database.length; i++){
+  if (database[i].day === dia){
+    verb = database[i].verb[rand]
+    adjective = database[i].adjective[rand]
+    noun = database[i].noun[rand]
+    console.log(`${nam} likes to ${verb} everyday like a ${adjective} ${noun}`)
+    
+    
+  }}})
