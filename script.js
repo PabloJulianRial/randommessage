0c1a7a9fc965d74fd2ae35bbb7dd515c933b826f
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2023-03-08 10:39:56 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//create database
+const database = {
+  Monday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump','ride', 'run', 'jump']},
+  Tuesday:  {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump','ride', 'run', 'jump']},
+  Wednesday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+  Thursday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+  Friday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+  Saturday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+  Sunday: {
+  verb:['ride', 'run', 'jump', 'ride', 'run', 'jump', 'ride', 'run', 'jump'] ,
+  adjective: ['fat', 'ugly', 'cold', 'exciting', 'amazing', 'beautiful'],
+  noun: ['car', 'dog', 'house', 'tree', 'ride', 'run', 'jump', 'ride', 'run', 'jump']},
+
+}
+console.log(database.Monday.verb[0])
